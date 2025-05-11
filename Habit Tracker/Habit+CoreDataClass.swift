4882b766fdfdd97d7a7ac192874663983e561e40
Elin <3ggapp24.tvinel@folkuniversitetet.nu>
2025-05-11 22:01:47 +0200

    Generated basic coredata structure

---
+++ Habit Tracker/Habit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Habit+CoreDataClass.swift
+//  Habit Tracker
+//
+//  Created by Elin Flyg on 2025-05-11.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Habit)
+public class Habit: NSManagedObject {
+
+}
